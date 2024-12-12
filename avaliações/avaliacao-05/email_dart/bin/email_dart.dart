b52325a850b23903af4bf05169fbc3c7ab1fad0a
ArthurDev-A <arthurdearaujocustodio0@gmail.com>
2024-12-12 01:10:32 -0300

    ativiade-05

--- avaliações/avaliacao-05/email_dart/bin/email_dart.dart
+++ avaliações/avaliacao-05/email_dart/bin/email_dart.dart
@@ -1,5 +1,30 @@
-import 'package:email_dart/email_dart.dart' as email_dart;
+import 'dart:io';
+import 'package:mailer/mailer.dart';
+import 'package:mailer/smtp_server/gmail.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${email_dart.calculate()}!');
+void main() async {
+  // Email do remetente
+  final email = "claudiaaraujo0809@gmail.com";
+
+  // Senha do remetente
+  final senha = "";
+
+  // Servidor SMTP do Gmail
+  final smtServer = gmail(email, senha);
+
+  // Cria a mensagem
+  final mensagem = Message()
+    ..from = Address(email, "Arthur De Araújo")
+    ..recipients.add("araujo.arthur61@aluno.ifce.edu.br")
+    ..subject = "Teste de envio"
+    ..text = "Este é um e-mail de teste";
+
+  try {
+    // Envia a mensagem
+    final sendReport = await send(mensagem, smtServer);
+    print("E-mail enviado com sucesso: ${sendReport.toString()}");
+  } on MailerException catch (e) {
+    // Tratamento de erro
+    print("Erro ao enviar o e-mail: ${e.toString()}");
+  }
 }
